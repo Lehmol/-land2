a74a252a8234881bf2cb42ae75dc8d6ff3ed1475
Lehmol <chalottelinnea@hotmail.com>
2025-06-03 14:55:12 +0200

    New styling to the home page and navbar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <nav className="navBar">
+      <a href="/">Hem</a>
+      <a href="/">Nyheter</a>
+      <a href="/">Styrelsen</a>
+      <a href="/">För medlemmar</a>
+      <button>Kontakta oss</button>
+    </nav>
+  );
+}
+
+export default Navigation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,19 @@
-import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
 
 function Home() {
   return (
     <>
-      <div className="homePage">
-        <div className="navContainer">
-          <nav className="navBar">
-            <a href="#">Hem</a>
-            <a href="#">Nyheter</a>
-            <a href="#">Dokument</a>
-            <a href="#">Kontakt</a>
-          </nav>
-        </div>
-        <main className="mainContent">
+      <Navigation />
+      <main className="mainContent">
+        <aside className="leftSide">
           <h1>Välkommen till Brf Öland 2</h1>
-          <div className="contentRow">
-            <div className="leftSide">
-              <p> Den bästa bostadsrättföreningen någonsin</p>
-            </div>
-            <div className="rightSide">
-              <img className="rightImg" src="./src/images/brf2.png" />
-            </div>
-          </div>
-        </main>
-      </div>
-      <Footer />
+          <br />
+          <p> - Ett tryggt boende med närhet till både stad och natur</p>
+        </aside>
+        <aside className="rightSide">
+          <img src="./src/images/6268.jpg" />
+        </aside>
+      </main>
     </>
   );
 }
